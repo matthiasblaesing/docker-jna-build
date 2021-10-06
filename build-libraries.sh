2414bc58a3a081ba09653912acfa443a7ea99378
Matthias Bläsing <mblaesing@doppel-helix.eu>
2021-10-06 20:23:52 +0200

    Rebuild configure where necessary

--- build-libraries.sh
+++ build-libraries.sh
@@ -13,12 +13,32 @@ for ARCH in $BIN_BUILDS; do
 
     BUILD_ENV_VAR="BUILD_ENV_$ARCH_VAR"
     BUILD_ENV=${!BUILD_ENV_VAR}
+
+    AUTORECONF=""
+
     mkdir -p /build/$BUILD_ENV/build-package
     mount -o bind /prep/build-packages/$BUILD_NAME /build/$BUILD_ENV/build-package
     if [ "$ARCH" = "arm" ]; then
         export QEMU_CPU=arm1176;
     fi
-    chroot /build/$BUILD_ENV /bin/bash -c 'cd build-package; export JAVA_HOME=/usr/lib/jvm/default-java; bash build.sh'
+
+    if [ "$ARCH" = "x86" ]; then
+        AUTORECONF="cd /build-package/native/libffi; autoreconf -fvi; cd ../..; ";
+    fi
+    
+    if [ "$ARCH" = "x86-64" ]; then
+        AUTORECONF="cd /build-package/native/libffi; autoreconf -fvi; cd ../..; ";
+    fi
+
+    if [ "$ARCH" = "armel" ]; then
+        AUTORECONF="cd /build-package/native/libffi; autoreconf -fvi; cd ../..; ";
+    fi
+
+    if [ "$ARCH" = "ppc" ]; then
+        AUTORECONF="cd /build-package/native/libffi; autoreconf -fvi; cd ../..; ";
+    fi
+
+    chroot /build/$BUILD_ENV /bin/bash -c "cd build-package; $AUTORECONF export JAVA_HOME=/usr/lib/jvm/default-java; bash build.sh"
     unset QEMU_CPU
     umount /build/$BUILD_ENV/build-package
 done
