c77fdd791e3e71381dadaf0302afa804de6aed00
Matthias Bläsing <mblaesing@doppel-helix.eu>
2023-03-15 22:05:47 +0100

    Ensure binfmt support is available when container is restarted

--- resetup.sh
+++ resetup.sh
@@ -1,6 +1,9 @@
 BASE_DIR="$(dirname "$0")"
 source $BASE_DIR/common.sh
 
+/etc/init.d/binfmt-support stop
+/etc/init.d/binfmt-support start
+
 # Installing the JDK needs proc file system mounted - so do this
 for i in $DEBIAN_BUILDS; do mountProc /build/debian-$i/proc ;done
 for i in $RASPBIAN_BUILDS; do mountProc /build/raspbian-$i/proc; done
